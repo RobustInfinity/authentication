e32ecbffb7a3c41243fc00f121a8d34bcb2199a7
mishraprince0786@gmail.com <mishraprince0786@gmail.com>
2018-08-22 23:18:32 +0530

    second changes

--- config/model.js
+++ config/model.js
@@ -1,5 +1,6 @@
 var {mongoose} = require('./connection');
 var {User} = require('./schema');
+var bcrypt = require('bcryptjs');
 
 var operations = {
     add : function(request, response, obj){
@@ -7,12 +8,57 @@ var operations = {
             email: obj.email,
             password: obj.password
         })
+        // var token = user.generateAuthToken();
         User.create(user)
         .then((result)=>{
             response.send(result);
-            console.log(result);
         }).catch((err)=>{
             response.status(400).send(err);
+            console.log(err);
+        })
+    },
+    message : function(message){
+        console.log(message)
+    },
+    login : function(request, response, obj){
+        User.findOne({
+            'email' : obj.email
+        },(err, user)=>{
+            if(user){
+            bcrypt.compare(obj.password, user.password,(err, success)=>{
+                if(success){
+                    console.log(user);
+                    var token = user.generateAuthToken();
+                    console.log(user);
+                    User.create(user)
+                    .then((result)=>{
+                        response.header('x-auth' , token).send({'email' : result.email});
+                    })
+                    .catch((err)=>{
+                        response.send(err); 
+                    })
+                }
+                else{
+                    response.send('Incorrect Password')
+                }
+                if(err){
+                    response.send(err);
+                }
+            }
+            )}
+            if(err){
+                response.send(err);
+            }
+        })
+    },
+    delete : function(request, response, user, token){
+        
+        user.removeToken(token)
+        .then((result)=>{
+            console.log(result);
+            response.send(result);
+        })
+        .catch((err)=>{
             console.log(err);
         })
     }
--- config/schema.js
+++ config/schema.js
@@ -1,6 +1,7 @@
-var {mongoose} = require('./connection');
-var validator = require('validator');
-
+const {mongoose} = require('./connection');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 var Schema = mongoose.Schema;
 
 
@@ -33,6 +34,57 @@ var UserSchema = new Schema({
     }]
 })
 
+
+UserSchema.methods.generateAuthToken = function(){
+    // var user = this;
+    var access = 'auth';
+    var token = jwt.sign({'_id': this._id.toHexString(),access},'abc123').toString();
+
+    this.tokens.push({access,token});
+
+    return token;
+}
+
+UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token,'abc123');
+        console.log(decoded);
+    } catch(e){
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id' : decoded._id,
+        'tokens.token' : token,
+        'tokens.access' : 'auth'
+    })
+}
+
+UserSchema.methods.removeToken = function(token){
+
+    return User.update({
+        $pull : {
+            tokens : [{token}]
+        }
+    })
+}
+
+UserSchema.pre('save',function(next){
+    // var user = this;
+    if(this.isModified('password')){
+        bcrypt.genSalt((err, salt)=>{
+            bcrypt.hash(this.password,salt,(err, hash)=>{
+                this.password = hash;
+                next();
+            })
+        })
+    }else{
+        next();
+    }
+})
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
